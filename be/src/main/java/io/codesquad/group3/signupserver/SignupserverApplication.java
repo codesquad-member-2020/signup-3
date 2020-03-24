4511e271664a8f4895ee1ba687c0b38d4fa3721d
David Chung <david215@berkeley.edu>
2020-03-24 15:57:24 +0900

    chore: 프로젝트 생성
    - Spring Boot:2.2.5
    - Spring Web
    - Spring Data JDBC
    - H2
    - Handlebars:0.3.0
    - JUnit:5.6.1
    - AssertJ:3.15.0
    - Logback:1.2.3

---
+++ be/src/main/java/io/codesquad/group3/signupserver/SignupserverApplication.java
@@ -0,0 +1,13 @@
+package io.codesquad.group3.signupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupserverApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/io/codesquad/group3/signupserver/SignupserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codesquad.group3.signupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
