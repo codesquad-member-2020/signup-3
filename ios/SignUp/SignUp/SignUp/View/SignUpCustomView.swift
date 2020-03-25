195c16c2e3a9ace5f67a5b915617decab317a42d
GangWoon <gangwoon2018@gmail.com>
2020-03-25 12:15:56 +0900

    add: SignUpCustomView class 생성
    mvc 패턴을 적용하기 위해서 view class를 생성했습니다.
    IBOutletCollection을 사용하지 않은 이유는 아웃렛 자체를 호출할 일이 빈번하기때문에 인덱스로 접근하는 것 보다 선언해두고 사용하는게 더 적합하다고 생각을 했습니다.

---
+++ ios/SignUp/SignUp/SignUp/View/SignUpCustomView.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpCustomView.swift
+//  SignUp
+//
+//  Created by Cloud on 2020/03/25.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+final class SignUpCustomView: UIView {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var identifierTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var idetnfierStatusLabel: UILabel!
+    @IBOutlet weak var passwordStatusLabel: UILabel!
+    
+    @IBOutlet weak var confirmPasswordStatusLabel: UILabel!
+    @IBOutlet weak var nameStatusLabel: UILabel!
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - IBActions
+    
+    // MARK: - Methods
+    
+}
