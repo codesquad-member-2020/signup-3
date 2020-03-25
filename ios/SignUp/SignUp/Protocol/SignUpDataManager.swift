f7a17c3269b77fc6a974614bf9a39a7507fd01ce
GangWoon <gangwoon2018@gmail.com>
2020-03-25 17:04:31 +0900

    chore: Model 객체 수정
    뷰에서 이벤트가 발생했을때 모델에서 처리하고 반영할 수 있도록 작성했습니다. 다시 돌아갈 방법은 completion Closure를 사용했습니다.
    정규식을 적용하지 않고 view에서 판단 가능한 부분은 모델에서 처리하지 않고 view에서 처리하도록 했습니다.

--- ios/SignUp/SignUp/Protocol/SignUpDataManager.swift
+++ ios/SignUp/SignUp/Protocol/SignUpDataManager.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class SignUpDataManager: NSObject {
+    private let identifierRegularExpression: String = "^[A-Za-z0-9_-]{5,20}$"
+    private let passwordRegularExpression: String = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,16}"
+}
+
+extension SignUpDataManager: RegularExpression {
+    func regex(_ text: String, kinds: String, completion: (Bool, String, String) -> Void) {
+        switch kinds {
+        case "identifier":
+            completion(
+                text.getArrayAfterRegex(regex: identifierRegularExpression).isEmpty,
+                "5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.",
+                "사용 가능한 아이디입니다."
+            )
+        default:
+            completion(
+                text.getArrayAfterRegex(regex: passwordRegularExpression).isEmpty,
+                "사용할 수 없는 비밀번호입니다.",
+                "사용 가능한 비밀번호입니다."
+            )
+        }
+    }
+}
