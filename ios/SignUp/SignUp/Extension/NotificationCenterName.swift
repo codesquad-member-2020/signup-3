385089ad7958eca6e521a7a26ebcdacf089fd19c
GangWoon <gangwoon2018@gmail.com>
2020-03-27 14:27:01 +0900

    chore: Notification.Name 전역 변수 생성

--- ios/SignUp/SignUp/Extension/NotificationCenterName.swift
+++ ios/SignUp/SignUp/Extension/NotificationCenterName.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+extension Notification.Name {
+    static var identifier: Notification.Name = Notification.Name("identifier")
+    static var password: Notification.Name = Notification.Name("password")
+    static var confirmPassword: Notification.Name = Notification.Name("confirmPassword")
+    static var name: Notification.Name = Notification.Name("name")
+    static var regularExpressionWithIdentifier: Notification.Name = Notification.Name("regularExpressionWithIdentifier")
+    static var regularExpressionWithPassword: Notification.Name = Notification.Name("regularExpressionWithPassword")
+    static var fetchPassword: Notification.Name = Notification.Name("fetchPassword")
+    static var pushPassword: Notification.Name = Notification.Name("pushPassword")
+    static var comparePasswords: Notification.Name = Notification.Name("comparePasswords")
+    static var isEmptyName: Notification.Name = Notification.Name("isEmptyName")
+    static var postEnabledButton: Notification.Name = Notification.Name("postPassword")
+    static var isEnabledButton: Notification.Name = Notification.Name("isEnabledButton")
+}
