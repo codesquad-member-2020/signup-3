79413207f19ff839b334e3acd619056c10054417
GangWoon <gangwoon2018@gmail.com>
2020-03-27 14:37:11 +0900

    feat: IdentifierTextField Delegate 생성
    identifierTextField의 문자열을 받아와서 유효성 검사를 하는 객체를 생성했습니다.
    여기서 고민했던 점이 SignUpViewModel 프로토콜 타입 변수를 갖을건지 아니면 매니저로 노티피케이션을 보낼지 고민했었습니다.
    둘다 이상하다는 느낌을 많이 받았지만 어떻게 해결할 방법을 몰라서 viewModel을 속성으로 갖는 방법을 선택했습니다.

--- ios/SignUp/SignUp/SignUp/View/IdentifierTextFieldDelegate.swift
+++ ios/SignUp/SignUp/SignUp/View/IdentifierTextFieldDelegate.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2020 Cloud. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class IdentifierTextFieldDelegate: NSObject {
+    weak var viewModel: SignUpViewModel?
+}
+
+extension IdentifierTextFieldDelegate: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+        textField.layer.borderWidth = 2.0
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        NotificationCenter.default.post(name: Notification.Name.identifier, object: nil)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        let result = viewModel?.applyIdentifierRegularExpression(text).0 ?? false
+        let success = viewModel?.applyIdentifierRegularExpression(text).1 ?? ""
+        let fail = viewModel?.applyIdentifierRegularExpression(text).2 ?? ""
+        NotificationCenter
+            .default
+            .post(
+                name: Notification.Name.regularExpressionWithIdentifier,
+                object: nil,
+                userInfo: result ? ["result": (true, success)] : ["result": (false, fail)]
+        )
+    }
+}
+
+
