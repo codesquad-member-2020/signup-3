a09daca78053827b6a59f887b49de9df626c1f84
GangWoon <gangwoon2018@gmail.com>
2020-03-27 14:38:36 +0900

    feat: PasswordTextField Delegate 생성
    passwordTextField의 문자열을 받아와서 유효성 검사를 하는 객체를 생성했습니다.

--- ios/SignUp/SignUp/SignUp/View/PasswordTextFieldDelegate.swift
+++ ios/SignUp/SignUp/SignUp/View/PasswordTextFieldDelegate.swift
@@ -1,9 +1,40 @@
 //
-//  PasswordTextFieldDelegate.swift
+//  IdentifierTextFieldDelegate.swift
 //  SignUp
 //
 //  Created by Cloud on 2020/03/26.
 //  Copyright © 2020 Cloud. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class PasswordTextFieldDelegate: NSObject {
+    weak var viewModel: SignUpViewModel?
+}
+
+extension PasswordTextFieldDelegate: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+        textField.layer.borderWidth = 2.0
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        NotificationCenter.default.post(name: Notification.Name.password, object: nil)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        let result = viewModel?.applyPasswordRegularExpression(text).0 ?? false
+        let success = viewModel?.applyPasswordRegularExpression(text).1 ?? ""
+        let fail = viewModel?.applyPasswordRegularExpression(text).2 ?? ""
+        NotificationCenter
+            .default
+            .post(
+                name: Notification.Name.regularExpressionWithPassword,
+                object: nil,
+                userInfo: result ? ["result": (true, success)] : ["result": (false, fail)]
+        )
+    }
+}
